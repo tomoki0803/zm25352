7c414a0ebe27ef9002ea71237b3dc3b99019e1b8
tomoki0803 <zm25352@josai.ac.jp>
2026-01-12 11:22:36 +0900

    Remove changephoto and resetphoto functions

--- ex1.js
+++ ex1.js
@@ -1,2 +1,1 @@
-function changephoto(){const img=document.getElementById("my-photo");img.src="image/image5.png";}
-function resetphoto(){const img=document.getElementById("my-photo");img.src="image/image6.jpg";}
+
