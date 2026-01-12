4abd42ef310abb1c4ca708a315f56592a8d95a24
tomoki0803 <zm25352@josai.ac.jp>
2026-01-12 11:21:32 +0900

    Update photo paths in changephoto and resetphoto functions

--- ex1.js
+++ ex1.js
@@ -1,2 +1,2 @@
-function changephoto(){const img=document.getElementById("my-photo");img.src="image/";}
-function resetphoto(){const img=document.getElementById("my-photo");img.src="image/jpg";}
+function changephoto(){const img=document.getElementById("my-photo");img.src="image/image5.png";}
+function resetphoto(){const img=document.getElementById("my-photo");img.src="image/image6.jpg";}
