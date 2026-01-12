8db265d0c97b569df68094bb3ee5744be135cb98
tomoki0803 <zm25352@josai.ac.jp>
2026-01-12 11:39:12 +0900

    Add functions to change images dynamically

--- ex1.js
+++ ex1.js
@@ -1,1 +1,5 @@
+function imagechange1()
+{document.getElementById('who613').innerHTML='<img class="imagechange" src="./image/image013.jpg" alt="オフ">';} 
 
+function imagechange2()
+{document.getElementById('who613').innerHTML='<img class="imagechange" src="./image/image00.jpg" alt="オン">';}
